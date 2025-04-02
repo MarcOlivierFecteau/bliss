60472f7ab9d3bfcaf29539e30f1c54086c9b7c3d
Marc-Olivier Fecteau <Marc-Olivier.Fecteau@cta-brp-udes.com>
2025-04-02 13:01:42 +0000

    Merged PR 258: Add rising/falling edge and 'delta' count to D-Pad
    Add rising/falling edge and 'delta' count to D-Pad
    
    NOTES:
    
    - The D-Pad indices are hard-coded, and tuned for an Xbox Series X
      controller: some modifications in the code MIGHT be required for
      proper mappings with a Logitech F710 controller.
    
    CHANGES:
    
    - Add 'dpad' field to `Bliss.msg`;
    - Add `dpad` field updates in `joy_callback`.

--- bliss/src/bliss.cpp
+++ bliss/src/bliss.cpp
@@ -4,36 +4,75 @@
 
 void Bliss::init_msgs(void)
 {
+    // Zero initialization to avoid undefined behavior on start
     msgin_ = prev_msgin_ = sensor_msgs::msg::Joy();
     bliss_ = prev_bliss_ = bliss_t();
 }
 
 void Bliss::joy_callback(const sensor_msgs::msg::Joy::SharedPtr msgin)
 {
+    // (Stupid) Memory management shenanigans to avoid segfaults
     prev_msgin_.axes.clear();
     prev_msgin_.buttons.clear();
     prev_msgin_.axes = msgin_.axes;
     prev_msgin_.buttons = msgin_.buttons;
+    prev_bliss_.axes.clear();
+    prev_bliss_ = bliss_;
+    bliss_.axes.clear();
+
+    // SHOULD only trigger on the first call
+    if (prev_msgin_.axes.empty()) {
+        prev_msgin_.axes.resize(msgin->axes.size());
+    }
     if (prev_msgin_.buttons.empty()) {
         prev_msgin_.buttons.resize(msgin->buttons.size());
     }
-    msgin_ = *msgin;
-
-    prev_timestamp_ = timestamp_;
-    timestamp_ = msgin_.header.stamp;
-
-    prev_bliss_ = bliss_;
+    if (prev_bliss_.axes.empty()) {
+        prev_bliss_.axes.resize(msgin->axes.size());
+    }
+    if (prev_bliss_.dpad.empty()) {
+        prev_bliss_.dpad.resize((size_t)2); // Assuming there is only one D-Pad
+    }
     if (prev_bliss_.buttons.empty()) {
         prev_bliss_.buttons.resize(msgin->buttons.size());
+    }
+    if (bliss_.axes.empty()) {
+        bliss_.axes.resize(msgin->axes.size());
+    }
+    if (bliss_.dpad.empty()) {
+        bliss_.dpad.resize((size_t)2); // Assuming there is only one D-Pad
     }
     if (bliss_.buttons.empty()) {
         bliss_.buttons.resize(msgin->buttons.size());
     }
+    // end: SHOULD only trigger on the first call
 
+    msgin_ = *msgin;
+    prev_timestamp_ = timestamp_;
+    timestamp_ = msgin_.header.stamp;
     bliss_.header.stamp = timestamp_;
 
     for (size_t i = 0; i < msgin_.axes.size(); ++i) {
-        bliss_.axes = msgin_.axes;
+        bliss_.axes.at(i) = msgin_.axes.at(i);
+        if (i == 6 || i == 7) { // D-Pad
+            size_t j = i - 6;
+            int32_t raw = msgin_.axes.at(i);
+            int32_t prev_raw = prev_msgin_.axes.at(i);
+            int32_t count = prev_bliss_.dpad[j].count; // Delta counter
+
+            bool rising_edge = (prev_raw == 0 && raw != 0);
+            bool falling_edge = (prev_raw != 0 && raw == 0);
+            static const bool toggle = false; // Toggle not supported
+            if (rising_edge) {
+                count += raw;
+            }
+
+            bliss_.dpad[j].raw = raw;
+            bliss_.dpad[j].count = count;
+            bliss_.dpad[j].rising_edge = rising_edge;
+            bliss_.dpad[j].falling_edge = falling_edge;
+            bliss_.dpad[j].toggle = toggle;
+        }
     }
 
     for (size_t i = 0; i < msgin_.buttons.size(); ++i) {
