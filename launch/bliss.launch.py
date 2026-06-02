06b4c8fe61e765467ee7c818f4a62cb3626546db
MarcOlivierFecteau <marc-olivier.fecteau@usherbrooke.ca>
2026-06-02 15:11:34 -0400

    Fix issues with LaunchConfiguration and launch arguments

--- launch/bliss.launch.py
+++ launch/bliss.launch.py
@@ -4,32 +4,31 @@ from launch_ros.actions import Node
 
 from launch import LaunchDescription
 
-node_name = LaunchConfiguration("node")
-
-node_name_cmd = DeclareLaunchArgument(
-    name="node",
-    default_value="joy",
-    description="The joy node to spin.",
-    choices=["joy", "game_controller"],
-)
-
-joy_node = Node(
-    package="joy",
-    executable="joy_node",
-    name=f"{node_name}_node",
-    output="screen",
-    parameters=[{"deadzone": 0.05}],
-)
-
-bliss_node = Node(
-    package="bliss",
-    executable="bliss_node",
-    name="bliss_node",
-    output="screen",
-)
-
 
 def generate_launch_description():
+    node_name = LaunchConfiguration("node")
+
+    node_name_cmd = DeclareLaunchArgument(
+        name="node",
+        default_value="joy",
+        description="The joy node to spin.",
+        choices=["joy", "game_controller"],
+    )
+
+    joy_node = Node(
+        package="joy",
+        executable="joy_node",
+        name=[node_name, "_node"],
+        output="screen",
+        parameters=[{"deadzone": 0.05}],
+    )
+
+    bliss_node = Node(
+        package="bliss",
+        executable="bliss_node",
+        name="bliss_node",
+        output="screen",
+    )
     return LaunchDescription(
         [
             node_name_cmd,
